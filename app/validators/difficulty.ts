d2446a506adad2c2f9f68d5b8f8beafd8409d9a2
Tom Gobich <tom@tomgobich.com>
2024-06-29 17:25:28 -0400

    clearing main branch, check other branches for example codebases

--- app/validators/difficulty.ts
+++
@@ -1,8 +0,0 @@
-import vine from '@vinejs/vine'
-
-export const difficultyValidator = vine.compile(
-  vine.object({
-    name: vine.string().maxLength(50),
-    color: vine.string().maxLength(20),
-  })
-)
